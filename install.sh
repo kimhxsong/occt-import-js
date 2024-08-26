549c7e4e8017366c34bba3cf006bb14d68a515b4
kimhxsong <kimhxsong@gmail.com>
2024-08-26 06:56:27 +0000

    setup: Add script to build 3rd-party dependencies

---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Create a directory named 'downloads' if it doesn't already exist
+# -p flag ensures that no error is raised if the directory already exists
+# Then, change the current working directory to the newly created 'downloads' directory
+mkdir -p downloads && cd ./downloads
+
+# Define installation and source directories based on the current working directory
+CURRENT_DIR=$(pwd)
+
+TCL_SRC_DIR="$CURRENT_DIR/tcl_source"
+TCL_INSTALL_DIR="$CURRENT_DIR/tcl_install"
+TK_SRC_DIR="$CURRENT_DIR/tk_source"
+TK_INSTALL_DIR="$CURRENT_DIR/tk_install"
+FREETYPE_SRC_DIR="$CURRENT_DIR/freetype_source"
+FREETYPE_INSTALL_DIR="$CURRENT_DIR/freetype_install"
+FREEIMAGE_SRC_DIR="$CURRENT_DIR/freeimage_source"
+FREEIMAGE_INSTALL_DIR="$CURRENT_DIR/freeimage_install"
+VTK_SRC_DIR="$CURRENT_DIR/vtk_source"
+VTK_INSTALL_PREFIX="$CURRENT_DIR/vtk_install"
+
+# Install Tcl
+echo "Installing Tcl..."
+cd $TCL_SRC_DIR/unix
+./configure --enable-gcc --enable-shared --enable-threads --enable-64bit  --prefix=$TCL_INSTALL_DIR 
+make
+make install
+
+# Install Tk
+echo "Installing Tk..."
+cd $TK_SRC_DIR/unix
+./configure --enable-gcc --enable-shared --enable-threads --with-tcl=$TCL_INSTALL_DIR/lib --prefix=$TK_INSTALL_DIR
+make
+make install
+
+# Install FreeType
+echo "Installing FreeType..."
+cd $FREETYPE_SRC_DIR
+./configure --prefix=$FREETYPE_INSTALL_DIR CFLAGS='-m64 -fPIC' CPPFLAGS='-m64 -fPIC'
+make
+make install
+
+# Install TBB
+echo "Installing TBB..."
+TBB_ARCHIVE="$CURRENT_DIR/oneapi-tbb-2021.5.0-lin.tgz"
+tar -xzf $TBB_ARCHIVE
+TBB_INSTALL_DIR="$CURRENT_DIR/tbb_install"
+cp -r oneapi-tbb-2021.5.0/* $TBB_INSTALL_DIR
+
+# Install FreeImage
+echo "Installing FreeImage..."
+cd $FREEIMAGE_SRC_DIR/Source/OpenEXR/Imath
+sed -i '60i#include <string.h>' ImathMatrix.h
+cd $FREEIMAGE_SRC_DIR
+# Modify Makefile if necessary
+sed -i 's|DESTDIR ?= /|DESTDIR ?= $(DESTDIR)|' Makefile.gnu
+sed -i 's|INCDIR ?= $(DESTDIR)/usr/include|INCDIR ?= $(DESTDIR)/include|' Makefile.gnu
+sed -i 's|INSTALLDIR ?= $(DESTDIR)/usr/lib|INSTALLDIR ?= $(DESTDIR)/lib|' Makefile.gnu
+sed -i 's|install  -m 644 -o root -g root $(HEADER) $(INCDIR)|install  -m 755 $(HEADER) $(INCDIR)|' Makefile.gnu
+sed -i 's|install  -m 644 -o root -g root $(STATICLIB) $(INSTALLDIR)|install  -m 755 $(STATICLIB) $(INSTALLDIR)|' Makefile.gnu
+sed -i 's|install  -m 644 -o root -g root $(SHAREDLIB) $(INSTALLDIR)|install  -m 755 $(SHAREDLIB) $(INSTALLDIR)|' Makefile.gnu
+sed -i 's|ldconfig|#ldconfig|' Makefile.gnu
+make
+make DESTDIR=$FREEIMAGE_INSTALL_DIR install
+
+# Clean temporary files for FreeImage
+make clean
+
+# Install VTK
+echo "Installing VTK..."
+cd $VTK_SRC_DIR
+cmake -DCMAKE_INSTALL_PREFIX=$VTK_INSTALL_PREFIX .
+make
+make install
+
+echo "Installation completed successfully."
